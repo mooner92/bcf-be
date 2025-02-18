1d2812a376283bb12c87952f3bb5ad90969cf0fc
Mooner92 <mooner92@kakao.com>
2025-02-18 23:36:08 +0900

    add

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -9,7 +9,7 @@ export class ChatService {
   // Chats 테이블에 메시지 저장
   async saveMessage(roomId: string, senderId: string, content: string) {
     const { data, error } = await this.supabase
-      .from('Chats')  // 테이블 이름: Chats
+      .from('Chatting')  // 테이블 이름: Chats
       .insert([{ room_id: roomId, sender_id: senderId, content }])
       .select('*')
       .single(); // 새로 추가된 한 건의 레코드를 반환
@@ -23,7 +23,7 @@ export class ChatService {
   // 특정 채팅방(room_id)에 해당하는 메시지 조회 (오름차순 정렬)
   async getMessages(roomId: string) {
     const { data, error } = await this.supabase
-      .from('Chats')
+      .from('Chatting')
       .select('*')
       .eq('room_id', roomId)
       .order('created_at', { ascending: true });
